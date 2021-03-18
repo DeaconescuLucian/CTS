afe3fff1585f7c1c2f251454c4ddaa37ce71d3ce
DeaconescuLucian <deaconesculucian18@stud.ase.ro>
2021-03-18 08:57:33 +0200

    ex

---
+++ seminar3/src/ro/ase/cts/individual/DL.java
@@ -0,0 +1,60 @@
+package ro.ase.cts.individual;
+
+import ro.ase.cts.individual.DL;
+
+public class DL {
+	
+	private int bileteCastigatoare;
+	private String pacaneauaPreferata;
+	private float celMaiMareMultiplicator;
+	public static DL instance=null;
+	
+	
+	
+	private DL(int bileteCastigatoare, String pacaneauaPreferata, float celMaiMareMultiplicator) {
+		this.bileteCastigatoare = bileteCastigatoare;
+		this.pacaneauaPreferata = pacaneauaPreferata;
+		this.celMaiMareMultiplicator = celMaiMareMultiplicator;
+	}
+
+	public static synchronized DL getInstance(int bileteCastigatoare, String pacaneauaPreferata, float celMaiMareMultiplicator)
+	{
+		if(instance==null)
+			instance=new DL(bileteCastigatoare,pacaneauaPreferata,celMaiMareMultiplicator);
+		return instance;
+	}
+
+	public int getBileteCastigatoare() {
+		return bileteCastigatoare;
+	}
+
+	public void setBileteCastigatoare(int bileteCastigatoare) {
+		this.bileteCastigatoare = bileteCastigatoare;
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return "DL [bileteCastigatoare=" + bileteCastigatoare + ", pacaneauaPreferata=" + pacaneauaPreferata
+				+ ", celMaiMareMultiplicator=" + celMaiMareMultiplicator + "]";
+	}
+
+	public String getPacaneauaPreferata() {
+		return pacaneauaPreferata;
+	}
+
+	public void setPacaneauaPreferata(String pacaneauaPreferata) {
+		this.pacaneauaPreferata = pacaneauaPreferata;
+	}
+
+	public float getCelMaiMareMultiplicator() {
+		return celMaiMareMultiplicator;
+	}
+
+	public void setCelMaiMareMultiplicator(float celMaiMareMultiplicator) {
+		this.celMaiMareMultiplicator = celMaiMareMultiplicator;
+	}
+}
+
+	
--- seminar3/src/ro/ase/cts/main/Program.java
+++ seminar3/src/ro/ase/cts/main/Program.java
@@ -1,6 +1,7 @@
 package ro.ase.cts.main;
 
 import ro.ase.cts.eager.ClinicaVeterinara;
+import ro.ase.cts.individual.DL;
 import ro.ase.cts.lazy.ClinicaVeterinaraLazy;
 
 public class Program {
@@ -24,6 +25,16 @@ public class Program {
 		
 		System.out.println(vet3.toString());
 		System.out.println(vet4.toString());
+		
+		DL dl1=DL.getInstance(40, "Dolphins Pearl Deluxe", 1500);
+		DL dl2=DL.getInstance(50, "Demon", 800);
+		
+		dl1.setPacaneauaPreferata("Book of RA");
+		dl2.setPacaneauaPreferata("Gems Bonanza");
+		
+		System.out.println(dl1.toString());
+		System.out.println(dl2.toString());
+		
 	}
 
 }
