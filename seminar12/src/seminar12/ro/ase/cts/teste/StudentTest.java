ba7a0c69b82275187a3d1b1d5446a5a33da3e703
DeaconescuLucian <deaconesculucian18@stud.ase.ro>
2021-05-20 09:24:00 +0300

    gfhjmndfv

--- seminar12/src/seminar12/ro/ase/cts/teste/StudentTest.java
+++ seminar12/src/seminar12/ro/ase/cts/teste/StudentTest.java
@@ -38,5 +38,78 @@ public class StudentTest {
 		student.adaugaNota(8);
 		assertEquals(1,student.getNote().size());
 	}
+	
+	@Test
+	public void testMedieMultipleNote() {
+		Student student=new Student();
+		int nota=8;
+		int nota2=9;
+		student.adaugaNota(nota);
+		student.adaugaNota(nota2);
+		assertEquals(8.5f,student.calculeazaMedie(),0.001f);
+		
+	}
+	
+	
+	@Test
+	public void testMedieFaraNote() {
+		Student student=new Student();
+		assertEquals(0f,student.calculeazaMedie(),0.001f);
+		
+	}
+	
+	@Test (expected=IndexOutOfBoundsException.class)
+	public void testVerificaGetNota() {
+		Student student=new Student();
+		int nota=8;
+		int nota2=9;
+		student.adaugaNota(nota);
+		student.adaugaNota(nota2);
+		assertEquals(nota,student.getNota(-1)) ;
+		
+	}
+	
+	@Test 
+	public void testVerificaGetNotaJU3() {
+		Student student=new Student();
+		int nota=8;
+		
+		student.adaugaNota(nota);
+		
+		try { int s=student.getNota(-1) ;
+		fail("Aici nu trebuie sa ajungem");}
+			catch(IndexOutOfBoundsException eroare) {
+				
+			}
+		
+		
+	}
+	
+	@Test
+	public void testVerificaRestanta() {
+		Student student=new Student();
+		int nota1=8;
+		int nota2=3;
+		int nota3=8;
+		student.adaugaNota(nota1);
+		student.adaugaNota(nota2);
+		student.adaugaNota(nota3);
+		
+		assertTrue(student.areRestante()) ;
+	}
+	
+	
+	@Test
+	public void testVerificaRestantaFalse() {
+		Student student=new Student();
+		int nota1=8;
+		int nota2=6;
+		int nota3=8;
+		student.adaugaNota(nota1);
+		student.adaugaNota(nota2);
+		student.adaugaNota(nota3);
+		
+		assertFalse(student.areRestante()) ;
+	}
 
 }
